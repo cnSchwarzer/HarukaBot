c8502621853ec3d8116e1f2b629078d34ddf8057
k1ee <cnschwarzer@qq.com>
2021-11-02 00:02:49 +0800

    Control

--- src/plugins/haruka_bot/plugins/cqhttp.py
+++ src/plugins/haruka_bot/plugins/cqhttp.py
@@ -1,4 +1,5 @@
 import asyncio
+import signal
 import traceback
 from datetime import datetime, timedelta
 from nonebot.log import logger
@@ -12,7 +13,8 @@ logger.success(f"go-cqhttp pid={cq.pid}")
 @scheduler.scheduled_job('interval', seconds=1800, id='cq')
 def cq_sched():
     global cq
-    cq.kill()
+    cq.send_signal(signal.CTRL_C_EVENT)
+    cq.send_signal(signal.SIGTERM)
     cq = subprocess.Popen("./go-cqhttp faststart")
     logger.success(f"go-cqhttp pid={cq.pid}")
 
