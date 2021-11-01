52bf01b9f34285e96084254b8bad851ab8fae528
k1ee <cnschwarzer@qq.com>
2021-11-01 23:40:50 +0800

    Control

--- src/plugins/haruka_bot/plugins/__init__.py
+++ src/plugins/haruka_bot/plugins/__init__.py
@@ -1,3 +1,3 @@
 from . import (at, dynamic, live, weibo, permission, pusher, sub, auto_agree,
-               auto_delete, help, fun)
+               auto_delete, help, fun, cqhttp)
 # TODO 将仅群可用指令的私聊拒绝部分分离成一个独立的handle
