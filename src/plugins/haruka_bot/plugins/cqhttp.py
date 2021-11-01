a8ac3a4761520b4ea785a5640643d1e241ca3dc0
k1ee <cnschwarzer@qq.com>
2021-11-02 00:05:12 +0800

    Control

--- src/plugins/haruka_bot/plugins/cqhttp.py
+++ src/plugins/haruka_bot/plugins/cqhttp.py
@@ -6,8 +6,9 @@ from nonebot.log import logger
 import subprocess
 from ..utils import scheduler
 
-cq = subprocess.Popen("./go-cqhttp faststart")
+cq = subprocess.Popen(["./go-cqhttp", 'faststart'])
 logger.success(f"go-cqhttp pid={cq.pid}")
+
 
 # restart cqhttp every 30 minutes
 @scheduler.scheduled_job('interval', seconds=1800, id='cq')
@@ -15,6 +16,5 @@ def cq_sched():
     global cq
     cq.send_signal(signal.CTRL_C_EVENT)
     cq.send_signal(signal.SIGTERM)
-    cq = subprocess.Popen("./go-cqhttp faststart")
+    cq = subprocess.Popen(["./go-cqhttp", 'faststart'])
     logger.success(f"go-cqhttp pid={cq.pid}")
-
