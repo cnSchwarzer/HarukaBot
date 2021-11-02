b390a2e94b74d147845bb02f6bbf229aed1eca22
k1ee <cnschwarzer@qq.com>
2021-11-02 10:01:48 +0800

    Control

--- src/plugins/haruka_bot/plugins/cqhttp.py
+++ src/plugins/haruka_bot/plugins/cqhttp.py
@@ -10,10 +10,10 @@ cq = subprocess.Popen(["./go-cqhttp", 'faststart'])
 logger.success(f"go-cqhttp pid={cq.pid}")
 
 
-# restart cqhttp every 30 minutes
-@scheduler.scheduled_job('interval', seconds=1800, id='cq')
-def cq_sched():
-    global cq
-    logger.info(f'killing go-cqhttp ret {subprocess.Popen(["kill", "-9", str(cq.pid)]).wait()}')
-    cq = subprocess.Popen(["./go-cqhttp", 'faststart'])
-    logger.success(f"go-cqhttp pid={cq.pid}")
+# # restart cqhttp every 30 minutes
+# @scheduler.scheduled_job('interval', seconds=1800, id='cq')
+# def cq_sched():
+#     global cq
+#     logger.info(f'killing go-cqhttp ret {subprocess.Popen(["kill", "-9", str(cq.pid)]).wait()}')
+#     cq = subprocess.Popen(["./go-cqhttp", 'faststart'])
+#     logger.success(f"go-cqhttp pid={cq.pid}")
